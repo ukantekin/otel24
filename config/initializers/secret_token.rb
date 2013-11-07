419e83167dca3bd288d1d3e3a463ee90fa7d3a43
ukantekin <ugurk1990@gmail.com>
2013-11-07 19:43:23 +0200

    Otel24 CRUD OPERATION

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c53f7b9c260a41f0b803d8128c99eb99f245165ebca410a8512353cb6f14bf438a5dc32428f7d3aec22712e7d41fab0d9dc723951750ec9791ab4c1267da23f7'
